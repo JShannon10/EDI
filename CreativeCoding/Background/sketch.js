737a1da5bc32b9a94de7e28a8dc72403da97c5bc
JShannon10 <113461556+JShannon10@users.noreply.github.com>
2023-01-12 12:51:11 -0500

    update 3

--- CreativeCoding/Background/sketch.js
+++ CreativeCoding/Background/sketch.js
@@ -1,7 +1,31 @@
-function setup() {
+let gif;
 
+ function preload(){
+
+   gif = loadImage ('Assets/BG_graphicpt.2.gif');
+ }
+
+function setup() {
+  createCanvas(windowWidth,windowWidth);
+  background(255,255,0);
 }
 
 function draw() {
+  fill(0,255,255);
+  strokeWeight(1);
+  var num = 20;
+  var sideLen = windowWidth/num;
 
+  translate(-150,-150);//give design bleed effect
+
+for (var y = 0; y < 2 * windowWidth;y += sideLen){
+  for(var x = 0; x< 2 * windowWidth;x += sideLen){
+      image (gif,x,y,sideLen,sideLen);
+  }
+}
+
+}
+
+function windowResized (){// This resize and refreshes the design with broswer
+  resizeCanvas (windowWidth,windowHeight);
 }
